a17e8e0bc80f7361385fa4198f04370daaf81b96
RAF <raf@MacBook-Pro-RAF.local>
2019-02-09 07:42:03 +0100

    combined reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,27 +2,40 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-
+import { bindActionCreators } from 'redux';
+import * as actionTypes from '../../store/actions';
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
+
+    
+
+    onChangeInputHandler=(event)=>{
+        console.log(event.target.value);
+        
+        //     const updatedOrderForm={
+        //         ...this.state.orderForm
+        //     }
+        //    const updatedFormElement= {...updatedOrderForm[inputIdentifier]}
+        //    updatedFormElement.value = event.target.value;
+        //    updatedFormElement.valid =this.checkValidity
+        //                         (updatedFormElement.value , updatedFormElement.validation);
+        //    updatedFormElement.touched = true; // checking if element was active for typing
+        //    updatedOrderForm[inputIdentifier]= updatedFormElement;
+           
+        //    let checkFormValidation = true;
+    
+        //    for(let inputIdentifier in updatedOrderForm){
+        //        checkFormValidation = updatedOrderForm[inputIdentifier].valid && checkFormValidation;
+               
+        //    }
+        // //    console.log(updatedOrderForm[inputIdentifier].valid);
+        //    this.setState({
+        //     orderForm: updatedOrderForm , 
+        //     formIsValid: checkFormValidation});
+        //    // console.log(updatedFormElement, updatedFormElement.value.length);
+        //    console.log(updatedFormElement);
+        // }
+
     }
 
     render () {
@@ -31,8 +44,16 @@ class Counter extends Component {
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Add" clicked={this.props.onAddCounter} changed={this.onChangeInputHandler} />
                 <CounterControl label="Subtract" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={()=>this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strRes=>(
+                        <li style={{cursor: 'pointer'}} key={strRes.id} onClick={()=>this.props.onDeleteResult(strRes.id)}>{strRes.value}</li>
+                    ))}
+                    
+                </ul>
             </div>
         );
     }
@@ -40,16 +61,22 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return{
-        ctr: state.counter
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+
+        //COMBINED REDUCERS MAKING NESTED OBJECTS FOR REDUCERS
     };
 };
 
 const mapDispatchToProps = dispatch =>{
     return{
-        onIncrementCounter: ()=>dispatch({type:'INCREMENT'}),
+        onIncrementCounter: ()=>dispatch({type: actionTypes.INCREMENT}),
         onDecrementCounter: ()=>dispatch({type: 'DECREMENT'}),
-        onAddCounter: ()=>dispatch({type: 'ADDVALUE', value: 5}),
-        onSubtractCounter: ()=>dispatch({type: 'SUBVALUE', value: -5})
+        onAddCounter: ()=>dispatch({type: 'ADDVALUE', value: 5 // or Payload
+    }),
+        onSubtractCounter: ()=>dispatch({type: 'SUBVALUE', value: -5}),
+        onStoreResult: (result)=>dispatch({type:actionTypes.STORE_RESULT, result: result}),
+        onDeleteResult: (id)=>dispatch({type:'DELETE_RESULT' , resEl: id}),
     }
 }
 
